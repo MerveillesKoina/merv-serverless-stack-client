4d72fee506a3419320022521b31611db71da692a
root <hndrxxmerv@yahoo.com>
2021-05-01 12:22:11 -0700

    Building my React App

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import MyNewNote from "./containers/MyNewNote";
+import MyNotes from "./containers/MyNotes";
+import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+
 
 export default function Routes() {
   return (
@@ -9,9 +18,25 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+	<UnauthenticatedRoute exact path="/login">
+	  <Login />
+	</UnauthenticatedRoute>
+	<UnauthenticatedRoute exact path="/signup">
+	  <Signup />
+	</UnauthenticatedRoute>
+	<AuthenticatedRoute exact path="/settings">
+	  <Settings />
+	</AuthenticatedRoute>
+	<AuthenticatedRoute exact path="/notes/new">
+	  <MyNewNote />
+	</AuthenticatedRoute>
+	<AuthenticatedRoute exact path="/notes/:id">
+	  <MyNotes />
+	</AuthenticatedRoute>
 	<Route>
 	  <NotFound />
 	</Route>
+
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 const config = {
   s3: {
-    REGION: "us-east-1
+    REGION: "us-east-1",
     BUCKET: "my-serverless-notes-app-upload",
   },
+MAX_ATTACHMENT_SIZE: 5000000,
+STRIPE_KEY: "pk_test_51IlU1JKjqiFafNFICO8zH2GWPPigmTvsVNfR8qM50xaLPplNh6fEiip1a7ZsbtNNKER5mr4h6FOXPr29FGYyGW8U00sGrjWeaG",
   apiGateway: {
     REGION: "us-east-1",
     URL: "https://qon366t7rk.execute-api.us-east-1.amazonaws.com/prod",
